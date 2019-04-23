463cdb1458f57f45fe8d9732423079cc05611a16
Freya Gehrke <freya.gehrke@xing.com>
2019-04-23 14:25:07 +0200

    Implement SongDetail

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SongList from './SongList'
+import SongDetail from './SongDetail'
 
 const App = props => {
   return (
@@ -7,6 +8,9 @@ const App = props => {
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
         </div>
       </div>
     </div>
